b19e44a10875d5879234a5ab5174048b8633ab06
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-04-03 21:52:42 +0300

    example 2

--- PracticalGuideToDragGestureRecognizer/examples/example2.swift
+++ PracticalGuideToDragGestureRecognizer/examples/example2.swift
@@ -9,7 +9,38 @@ import SwiftUI
 
 struct example2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Example2()
+    }
+}
+
+struct Example2: View {
+    @GestureState private var isMoving = false
+    @State private var rectPosition = CGPoint(x: 100, y: 100)
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Circle()
+                    .fill(isMoving ? Color.red : Color.green)
+                    .frame(width: 50, height: 50, alignment: .center)
+                Spacer()
+            }
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.blue)
+                .frame(width: 100, height: 100, alignment: .center)
+                .position(rectPosition)
+                .gesture(
+                    DragGesture()
+                        .onChanged({ (value) in
+                            self.rectPosition = value.location
+                        })
+                        .updating($isMoving, body: { (value, state, transaction) in
+                            state = true
+                        })
+                )
+        }
     }
 }
 
