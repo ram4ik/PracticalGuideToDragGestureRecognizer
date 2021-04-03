cf3b53c33c9c20c839ead63df1ace87b909009ef
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-04-03 22:27:40 +0300

    example 4

--- PracticalGuideToDragGestureRecognizer/examples/example4.swift
+++ PracticalGuideToDragGestureRecognizer/examples/example4.swift
@@ -9,7 +9,54 @@ import SwiftUI
 
 struct example4: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Example4()
+    }
+}
+
+struct Example4: View {
+    @State private var sliderPosition: CGFloat = 50 - UIScreen.main.bounds.width
+    @GestureState private var offset = CGSize.zero
+    
+    var body: some View {
+        GeometryReader { geoProxy in
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(systemName: "line.horizontal.3")
+                        .rotationEffect(.degrees(90))
+                }
+                .foregroundColor(.white)
+                .frame(height: 100)
+                .padding()
+                .background(
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.orange)
+                            .offset(x: -100)
+                        
+                        Capsule()
+                            .fill(Color.orange)
+                    }
+                )
+                .offset(x: sliderPosition + offset.width - 10)
+                .gesture(
+                    DragGesture()
+                        .updating($offset, body: { (value, state, transaction) in
+                            state = value.translation
+                        })
+                        .onEnded({ (value) in
+                            if value.translation.width < -geoProxy.size.width * 0.4 {
+                                sliderPosition = 50 - geoProxy.size.width
+                            } else {
+                                sliderPosition = 0
+                            }
+                        })
+                ).animation(.spring())
+                
+                Spacer()
+            }
+        }
     }
 }
 
