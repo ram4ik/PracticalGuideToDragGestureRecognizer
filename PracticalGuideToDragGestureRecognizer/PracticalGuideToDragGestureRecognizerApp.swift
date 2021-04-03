26a9d7771d9547fe05b0892e6edd5f60287b835c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-04-03 21:32:25 +0300

    Initial Commit

---
+++ PracticalGuideToDragGestureRecognizer/PracticalGuideToDragGestureRecognizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticalGuideToDragGestureRecognizerApp.swift
+//  PracticalGuideToDragGestureRecognizer
+//
+//  Created by ramil on 03.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct PracticalGuideToDragGestureRecognizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
