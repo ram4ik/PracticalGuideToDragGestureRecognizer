13af936b844fa592641091a3174d88f5eb12c30c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-04-04 11:28:11 +0300

    Example6

--- PracticalGuideToDragGestureRecognizer/examples/example6.swift
+++ PracticalGuideToDragGestureRecognizer/examples/example6.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct example6: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Example6()
+    }
+}
+
+struct Example6: View {
+    @State private var angle: Angle = .zero
+    @State private var offset: CGSize = .zero
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.orange)
+                .frame(width: 20, height: 20)
+            
+            Image(systemName: "arrow.right.circle.fill")
+                .foregroundColor(Color.red)
+                .font(.largeTitle)
+                .rotationEffect(angle)
+                .offset(offset)
+                .gesture(
+                    DragGesture(minimumDistance: 50, coordinateSpace: .local)
+                        .onChanged({ (value) in
+                            let translation = value.translation
+                            let point1 = Double(translation.width == 0 ? 0 : translation.width)
+                            let point2 = Double(translation.height)
+                            let a = point1 < 0 ? atan(Double(point2 / point1)) : atan(Double(point2 / point1)) - Double.pi
+                            
+                            angle = Angle(radians: a)
+                            offset = value.translation
+                        })
+                        .onEnded({ (value) in
+                            angle = .zero
+                            offset = .zero
+                        })
+                ).animation(.spring())
+        }
     }
 }
 
